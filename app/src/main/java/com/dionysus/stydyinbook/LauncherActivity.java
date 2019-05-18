650305d824c511e5f40c63ea8563818f0279bec9
zhaixuan <zxwm2@126.com>
2019-05-19 00:10:25 +0800

    【尚硅谷】【第一讲：软件框架的搭建】【启动页面】

---
+++ app/src/main/java/com/dionysus/stydyinbook/LauncherActivity.java
@@ -0,0 +1,14 @@
+package com.dionysus.stydyinbook;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LauncherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
