4409d5f5bac5bd9a56d5b8b56a86635104408d96
zhaixuan <zxwm2@126.com>
2019-05-19 01:16:22 +0800

    【尚硅谷】【第一讲：Android软件框架搭建】【主页面布局】

--- app/src/main/java/com/dionysus/stydyinbook/LauncherActivity.java
+++ app/src/main/java/com/dionysus/stydyinbook/LauncherActivity.java
@@ -1,6 +1,8 @@
 package com.dionysus.stydyinbook;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +12,22 @@ public class LauncherActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //在主线程中执行
+                startMainActivity();
+            }
+        }, 2000);
+    }
+
+    /**
+     * 启动主页面
+     */
+    private void startMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        //关闭当前页面
+        finish();
     }
 }
--- app/src/main/java/com/dionysus/stydyinbook/MainActivity.java
+++ app/src/main/java/com/dionysus/stydyinbook/MainActivity.java
@@ -1,13 +1,19 @@
 package com.dionysus.stydyinbook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup mRgMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRgMain = (RadioGroup) findViewById(R.id.rg_main);
+        // 设置默认选中常用框架
+        mRgMain.check(R.id.rb_common_frame);
     }
 }
