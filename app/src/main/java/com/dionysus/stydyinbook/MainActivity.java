eed9eaa5c4394b57cd57039567b6a55d37dc80d6
zhaixuan <zxwm2@126.com>
2019-05-19 10:31:55 +0800

    【尚硅谷】【第一讲：Android软件框架搭建】【设置底部按钮的监听事件】

--- app/src/main/java/com/dionysus/stydyinbook/MainActivity.java
+++ app/src/main/java/com/dionysus/stydyinbook/MainActivity.java
@@ -2,6 +2,8 @@ package com.dionysus.stydyinbook;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.widget.RadioGroup;
 
 import com.dionysus.stydyinbook.base.BaseFragment;
@@ -17,6 +19,10 @@ public class MainActivity extends FragmentActivity {
 
     private RadioGroup mRgMain;
     private List<BaseFragment> mBaseFragment;
+    /**
+     * 选中Fragment对应的位置
+     */
+    private int miPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,67 @@ public class MainActivity extends FragmentActivity {
         initView();
         // 初始化Fragment
         initFragment();
+        // 设置RadioGroup的监听
+        setListener();
+    }
+
+    private void setListener() {
+        mRgMain.setOnCheckedChangeListener(new MyOnCheckedListener());
+        // 设置默认选中常用框架
+        mRgMain.check(R.id.rb_common_frame);
+    }
+
+    class MyOnCheckedListener implements RadioGroup.OnCheckedChangeListener {
+
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            switch (checkedId) {
+                //常用框架
+                case R.id.rb_common_frame:
+                    miPosition = 0;
+                    break;
+                //第三方
+                case R.id.rb_thirdparty:
+                    miPosition = 1;
+                    break;
+                //自定义
+                case R.id.rb_custom:
+                    miPosition = 2;
+                    break;
+                //其他
+                case R.id.rb_other:
+                    miPosition = 3;
+                    break;
+                default:
+                    miPosition = 0;
+                    break;
+            }
+            //根据位置得到对应的Fragment
+            BaseFragment fragment = getFragment();
+            //替换
+            switchFragment(fragment);
+        }
+    }
+
+    private void switchFragment(BaseFragment fragment) {
+        //1.得到FragmentManager
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        //2.开启事务
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        //3.替换
+        transaction.replace(R.id.fl_content, fragment);
+        //4.提交
+        transaction.commit();
+    }
+
+    /**
+     * 根据位置得到对应的Fragment
+     *
+     * @return
+     */
+    private BaseFragment getFragment() {
+        BaseFragment fragment = mBaseFragment.get(miPosition);
+        return fragment;
     }
 
     private void initFragment() {
@@ -41,8 +108,6 @@ public class MainActivity extends FragmentActivity {
 
     private void initView() {
         setContentView(R.layout.activity_main);
-        mRgMain = (RadioGroup) findViewById(R.id.rg_main);
-        // 设置默认选中常用框架
-        mRgMain.check(R.id.rb_common_frame);
+        mRgMain = findViewById(R.id.rg_main);
     }
 }
