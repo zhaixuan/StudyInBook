3a1dfff2834d502017d33ed3f37659f480c977b0
zhaixuan <zxwm2@126.com>
2019-05-21 23:19:49 +0800

    【尚硅谷】【第三讲：Json数据解析】【将 JSON 格式的字符串数组转换为 List】

--- app/src/main/java/com/dionysus/stydyinbook/json/activity/NativeJsonParseActivity.java
+++ app/src/main/java/com/dionysus/stydyinbook/json/activity/NativeJsonParseActivity.java
@@ -10,8 +10,12 @@ import android.widget.TextView;
 import com.dionysus.stydyinbook.R;
 import com.dionysus.stydyinbook.json.bean.ShopInfo;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 手动JSON解析页面
@@ -77,18 +81,65 @@ public class NativeJsonParseActivity extends AppCompatActivity implements View.O
                 //（1）将json格式的字符串{}转换为Java对象
                 jsoonToJavaObjectByNative();
                 break;
-            //（2）将json格式的字符串[]转换为Java对象的List
             case R.id.btn_native_to_javalist:
+                //（2）将json格式的字符串[]转换为Java对象的List
+                jsonToJavaListByNative();
                 break;
-            //（3）复杂json数据解析
             case R.id.btn_native_complex:
+                //（3）复杂json数据解析
                 break;
-            //（4）特殊json数据解析
             case R.id.btn_native_special:
+                //（4）特殊json数据解析
                 break;
             default:
                 break;
         }
+    }
+
+    /**
+     * 将json格式的字符串[]转换为Java对象的List
+     */
+    private void jsonToJavaListByNative() {
+        // 1.获取或创建Json数据
+        String json = "[\n" +
+                "    {\n" +
+                "        \"id\": 1,\n" +
+                "        \"imagePath\": \"http://192.168.10.165:8080/f1.jpg\",\n" +
+                "        \"name\": \"大虾1\",\n" +
+                "        \"price\": 12.3\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 2,\n" +
+                "        \"imagePath\": \"http://192.168.10.165:8080/f2.jpg\",\n" +
+                "        \"name\": \"大虾2\",\n" +
+                "        \"price\": 12.5\n" +
+                "    }\n" +
+                "]";
+
+        List<ShopInfo> shops = new ArrayList<>();
+        // 2.解析数据
+        try {
+            JSONArray jsonArray = new JSONArray(json);
+            for (int i = 0; i < json.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                if (null != jsonObject) {
+                    int id = jsonObject.optInt("id");
+                    String name = jsonObject.optString("name");
+                    double price = jsonObject.optDouble("price");
+                    String imagePath = jsonObject.optString("imagePath");
+                    // 封装java对象
+                    ShopInfo shopInfo = new ShopInfo(id, name, price, imagePath);
+                    shops.add(shopInfo);
+
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        // 3.显示Json数据
+        mtxtOriginal.setText(json);
+        mtxtLast.setText(shops.toString());
+
     }
 
     /**
@@ -109,6 +160,7 @@ public class NativeJsonParseActivity extends AppCompatActivity implements View.O
             String name = jsonObject.optString("name");
             double price = jsonObject.optDouble("price");
             String imagePath = jsonObject.optString("imagePath");
+            // 封装java对象
             shopInfo = new ShopInfo(id, name, price, imagePath);
         } catch (JSONException e) {
             e.printStackTrace();
