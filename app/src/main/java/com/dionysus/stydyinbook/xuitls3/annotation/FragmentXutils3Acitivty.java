00e7db8143a0fcc4d4dc14e0e0037c4c67b929da
zhaixuan <zxwm2@126.com>
2019-05-23 01:14:49 +0800

    【尚硅谷】【第六讲：xUtils3注解框架】【在 Fragment 中使用注解初始化布局】

--- app/src/main/java/com/dionysus/stydyinbook/xuitls3/annotation/FragmentXutils3Acitivty.java
+++ app/src/main/java/com/dionysus/stydyinbook/xuitls3/annotation/FragmentXutils3Acitivty.java
@@ -2,9 +2,12 @@ package com.dionysus.stydyinbook.xuitls3.annotation;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.widget.TextView;
 
 import com.dionysus.stydyinbook.R;
+import com.dionysus.stydyinbook.xuitls3.annotation.fragment.DemoFragment;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
@@ -22,5 +25,13 @@ public class FragmentXutils3Acitivty extends FragmentActivity {
         x.view().inject(this);
         //设置标题
         mtxtTitle.setText(getString(R.string.xutils3fragment_txt_title_name));
+        //1.得到FragmentManager
+        FragmentManager fm = getSupportFragmentManager();
+        //2.开启事务
+        FragmentTransaction transaction = fm.beginTransaction();
+        //3.替换Fragment
+        transaction.replace(R.id.fl_content, new DemoFragment());
+        //4.提交
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/dionysus/stydyinbook/xuitls3/annotation/fragment/DemoFragment.java
@@ -0,0 +1,62 @@
+package com.dionysus.stydyinbook.xuitls3.annotation.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dionysus.stydyinbook.R;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
+
+/**
+ * @className: DemoFragment
+ * @author: Dionysus
+ * @date: 2019/5/23 1:01
+ * @Description: DemoFragment
+ */
+@ContentView(R.layout.fragment_demo)
+public class DemoFragment extends Fragment {
+
+    @ViewInject(R.id.btn_fragment)
+    private Button mbtnFragment;
+    @ViewInject(R.id.txt_text)
+    private TextView mtxtText;
+    private Context mContext;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getContext();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return x.view().inject(this, inflater, container);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mbtnFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "点击已生效", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mtxtText.setText("已初始化");
+    }
+}
+
